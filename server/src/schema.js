e764ee5bec96b8ffd9e5a0338cbeaab7d9b9349d
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-20 17:49:36 -0800

    Update schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,10 +4,15 @@ const typeDefs = gql`
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "The track's title"
     title: String!
+    "The track's main author"
     author: Author!
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
   }
 `;
