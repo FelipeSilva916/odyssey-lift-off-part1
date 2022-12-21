ce1344ae5adc2cdecfd0cc4123e978976c4fdc27
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-20 17:57:19 -0800

    Update schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,11 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -14,6 +19,15 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
   }
 `;
 
