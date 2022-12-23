728e717c942a5c998f1a428e19721012e60a51e3
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-22 16:32:35 -0800

    new kind

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <Pages />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
