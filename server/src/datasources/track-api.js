b1350f4a6598e686d659537ae00f335576a773af
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-23 08:01:50 -0800

    8

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,31 +1,18 @@
-const { RESTDatasource } = require("apollo-datasource-rest");
+const { RESTDataSource } = require("apollo-datasource-rest");
 
-class TrackAPI extends RESTDatasource {
+class TrackAPI extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
   }
+
   getTracksForHome() {
     return this.get("tracks");
   }
 
   getAuthor(authorId) {
-    return this.get(`authors/${authorId}`);
+    return this.get(`author/${authorId}`);
   }
 }
 
 module.exports = TrackAPI;
-
-class SpaceCatsAPI extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL = "https://api.spacexdata.com/v4/";
-  }
-  getSpaceCats() {
-    return this.get("spaceCats");
-  }
-
-  getMissions(catId) {
-    return this.get(`spaceCats/${catId}/missions`);
-  }
-}
